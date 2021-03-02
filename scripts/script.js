6bc7e21f7fd88acc0d268ebd31190843575e2df3
Wrik Das <wrikdas22@gmail.com>
2021-03-02 23:16:15 +0530

    Implemented core functionality and operations of calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,23 @@
-let isFloat = false;
+// useful variables
 
+let operation = '';
+let prevOperand = '';
+let isFloat = false;
+let isLastInputOperation = false;
+
+// display node
 const display = document.querySelector('#display');
+
+// event listeners
 
 const numbers = [ ...document.querySelectorAll('.number') ];
 numbers.forEach(number => number.addEventListener('click', handleOperand));
+
+const operations = [ ...document.querySelectorAll('.operation') ];
+operations.forEach(operation => operation.addEventListener('click', handleOperator));
+
+const equal = document.querySelector('#eq');
+equal.addEventListener('click', evaluate);
 
 const clear = document.querySelector('#clr');
 clear.addEventListener('click', deleteLast);
@@ -11,28 +25,84 @@ clear.addEventListener('click', deleteLast);
 const allClear = document.querySelector('#acl');
 allClear.addEventListener('click', deleteAll);
 
-
-function deleteLast(event) {
-    display.innerHTML = display.innerHTML.slice(0, display.innerHTML.length - 1);
-}
-
+// clear all
 function deleteAll(event) {
+    operation = '';
+    isFloat = false;
+    prevOperand = '';
+    isLastInputOperation = false;
     display.innerHTML = '';
 }
 
-function handleOperand(event) {
-    let num = event.target.innerHTML;
-    if (isFloat && num === '.') return;
-    if (num == '.') {
-        isFloat = true;
+// delete last char
+function deleteLast(event) {
+    if (isLastInputOperation) operation = '';
+    let operand = display.innerHTML;
+    if (operand.slice(-1) === '.') {
+        isFloat = false;
     }
-    display.innerHTML += num;
+    display.innerHTML = operand.slice(0, display.innerHTML.length - 1);
 }
+
+
+// sets operation and handles
+// prev operation if it exists
+function handleOperator(event) {
+    if (prevOperand.length > 0) {
+        evaluate(event);
+    }
+    prevOperand = display.innerHTML;
+    operation = event.target.innerHTML;
+    isLastInputOperation = true;
+}
+
+// handles number/dot input
+// only single decimal point allowed
+function handleOperand(event) {
+    if (isLastInputOperation) {
+        clearScreen();
+    }
+    const operand = event.target.innerHTML;
+    if (isFloat && operand === '.') return;
+    if (operand == '.') isFloat = true;
+    display.innerHTML += operand;
+}
+
+// evaluates the current
+// and prev operands
+function evaluate(event) {
+    if (prevOperand.length == 0 || isLastInputOperation) return;
+
+    const currOperand = display.innerHTML;
+    const result = operate(operation,
+        Number(prevOperand),
+        Number(currOperand));
+
+    isFloat = isInt(result)? false : true;
+    display.innerHTML = `${result ?? ''}`;
+    prevOperand = '';
+}
+
+// Decimal point checker
+function isInt(num) {
+    return (num % 1 === 0);
+}
+
+// clears screen but
+// preserves operation/operands
+function clearScreen() {
+    isFloat = false;
+    isLastInputOperation = false;
+    display.innerHTML = '';
+}
+
 
 // rounds to 3 places
 function round(num) {
     return Math.floor(num * 1000) / 1000;
 }
+
+// calculator functions
 
 function add(a, b) {
     return round(a + b);
@@ -47,33 +117,19 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        alert('You can\'t divide by 0!');
+        return;
+    }
     return round(a / b);
 }
 
-function mod(a, b) {
-    return round(a % b);
-}
-
+// caller
 function operate(operator, a, b) {
     switch(operator) {
-        case '+':
-            add(a, b);
-            break;
-
-        case '-':
-            subtract(a, b);
-            break;
-
-        case '×':
-            multiply(a, b);
-            break;
-
-        case '÷':
-            divide(a, b);
-            break;
-
-        case '%':
-            mod(a, b);
-            break;
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '×': return multiply(a, b);
+        case '÷': return divide(a, b);
     }
 }
